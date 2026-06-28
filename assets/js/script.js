8058fd6da8f5dbe33c574f79ca86505f2d0d26fc
m1buildingllc <asaturasatryan98@gmail.com>
2026-06-28 14:21:18 +0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,15 +17,3 @@ faqItems.forEach(item => {
 
 
 
-const nav = document.getElementById("mainMenu");
-
-menuToggle.addEventListener("click", () => {
-    nav.classList.toggle("active");
-});
-
-
-document.querySelectorAll("#mainMenu a").forEach(link => {
-    link.addEventListener("click", () => {
-        nav.classList.remove("active");
-    });
-});
